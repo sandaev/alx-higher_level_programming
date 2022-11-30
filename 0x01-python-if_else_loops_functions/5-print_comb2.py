b274fa1a8d1802782fa0adb6dbf19c6130e705ce
Sanni Ozigi <sandaev20@gmail.com>
2022-11-30 03:09:51 -0800

    edited print function string format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if (num < 99):
-        print(f"{num:02}", end=", ")
+        print('{num:02d}'.format(num = num), end=", ")
     else:
-        print(f"{num}")
+        print('{}'.format(num))
