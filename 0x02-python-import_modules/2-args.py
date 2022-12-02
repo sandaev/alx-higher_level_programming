e140da4c79804c966ee89659dd18ad4fc6d3dd6f
Sanni Ozigi <sandaev20@gmail.com>
2022-12-01 20:10:45 -0800

    fix typo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         for i in range(argv_len):
             print('{}: {}'.format((i + 1), sys.argv[i + 1]))
     else:
-        print('{} arguments'.format(argv_len))
+        print('{} arguments.'.format(argv_len))
