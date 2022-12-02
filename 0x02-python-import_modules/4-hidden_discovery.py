dd835dc8d7cd7d103544f94d11a53bd33913850a
Sanni Ozigi <sandaev20@gmail.com>
2022-12-01 20:22:46 -0800

    error fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from builtins import dir
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     names = dir("hidden_4.pyc")
     len_names = len((dir("hidden_4.pyc")))
     for i in range(len_names):
