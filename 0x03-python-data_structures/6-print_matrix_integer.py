0955eb8ef6bd1458abc18b5340e9959a64fe77a1
Sanni Ozigi <sandaev20@gmail.com>
2022-12-04 07:28:21 -0800

    Change logic for newline

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     if len(matrix) > 0:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end=" ")
-            print()
+                if j == len(matrix[i]) - 1:
+                    print("{:d}".format(matrix[i][j]))
+                else:
+                    print("{:d}".format(matrix[i][j]), end=" ")
