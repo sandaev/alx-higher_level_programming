d16e695e09829ef07319562fda636f6062cbc743
Sanni Ozigi <sandaev20@gmail.com>
2022-12-04 06:33:55 -0800

    empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -9,5 +9,8 @@
 
 def print_reversed_list_integer(my_list=[]):
 
-    for i in range(-1, -len(my_list) - 1, -1):
-        print("{:d}".format(my_list[i]))
+    if len(my_list) == 0:
+        print("{}".format(my_list))
+    else:
+        for i in range(-1, -len(my_list) - 1, -1):
+            print("{:d}".format(my_list[i]))
