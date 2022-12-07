74c992623ae393ecbe8ed84cefa130cf936c1708
Sanni Ozigi <sandaev20@gmail.com>
2022-12-07 07:52:22 -0800

    search_replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if len(my_list) != 0:
+        new_list = []
+        for i in range(len(my_list)):
+            if my_list[i] != search:
+                new_list.append(my_list[i])
+            else:
+                new_list.append(replace)
+        return (new_list)
+    return (my_list)
