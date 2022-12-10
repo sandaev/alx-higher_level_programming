c8a566d871f22cb97ae1ea2c3f5b42eeb74dede7
Sanni Ozigi <sandaev20@gmail.com>
2022-12-10 10:47:40 -0800

    value error fix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     
     if value in a_dictionary.values():
-        for k, v in list(a_dictionary.keys()):
+        for k in list(a_dictionary.keys()):
             if v == value:
                 del a_dictionary[k]
         return (a_dictionary)
