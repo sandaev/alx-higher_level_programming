b3d315c4fd0d08ac26fdd260a731d35288381c5c
Sanni Ozigi <sandaev20@gmail.com>
2022-12-10 10:44:30 -0800

    fix logic error

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,7 +3,7 @@ def complex_delete(a_dictionary, value):
     
     if value in a_dictionary.values():
         for k, v in list(a_dictionary.keys()):
-            if v != value:
+            if v == value:
                 del a_dictionary[k]
         return (a_dictionary)
     return (a_dictionary)
