2027e74c214f82df4b44c240436d7d657eb4dbce
Sanni Ozigi <sandaev20@gmail.com>
2022-12-10 04:48:52 -0800

    add if

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    return ({k: v for k, v in a_dictionary.items() if v != value})
+    
+    if value in a_dictionary.values():
+        return ({k: v for k, v in a_dictionary.items() if v != value})
+    return (a_dictionary)
