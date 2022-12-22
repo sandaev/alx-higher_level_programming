162614e14c7d2f9889c5b4cb3e5299a5974c314c
Sanni Ozigi <sandaev20@gmail.com>
2022-12-21 21:12:49 -0800

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Contains a class that defines a square by size'''
+
+
+class Square:
+    '''Class that defines a square by size
+
+    Attributes:
+
+    __size(any type) - Private attribute that determines the length of square
+    '''
+
+    def __init__(self, __size):
+        '''Constructor for the saure class
+
+        Attributes:
+
+        _Square__size(no type) - length of square
+        '''
+
+        self._Square__size = __size
