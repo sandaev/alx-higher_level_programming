af274acdba62f28e10e0fec33c71958792f886d0
Sanni Ozigi <sandaev20@gmail.com>
2023-01-09 11:14:34 -0800

    python inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" DEFINES A FUNCTION THAT RETURNS A LIST OFF ALL ATTRIBUTES
+    AND METHODS OF AN OBJECT
+"""
+
+
+def lookup(obj):
+    """ Returns a list of all attribiutes and methods """
+    return dir(obj)
