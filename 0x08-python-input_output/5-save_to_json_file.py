1a4c336faf1660d7c53faedfd1996a000a731936
Sanni Ozigi <sandaev20@gmail.com>
2023-01-10 07:02:20 -0800

    save json

---
+++ 0x08-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" FUNCTION THAT WRITES AN OBJECT TO A TEXT
+    FILE, USING JSON REPRESENTATION
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """ Writes an object to a text file, using json rep """
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
