d938a4350bff4b52dbccb469b4dd8a0c04a9d641
Sanni Ozigi <sandaev20@gmail.com>
2023-01-09 20:57:16 -0800

    basegeometry with method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines an basegeometry class"""
+
+
+class BaseGeometry:
+    """ An empty basegeometry class """
+    def area(self):
+        raise Exception("area() is not implemented")
