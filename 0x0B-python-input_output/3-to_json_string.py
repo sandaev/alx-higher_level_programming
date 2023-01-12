e9472bf0fdd58cdece9694d7cb39b2f5908f06c9
Sanni Ozigi <sandaev20@gmail.com>
2023-01-11 23:58:55 -0800

    fix error

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,8 +2,9 @@
 """ FUNCTION THAT RETURNS THE JSON REPRESENTATION OF AN
     OBJECT
 """
+import json
 
 
 def to_json_string(my_obj):
     """ Returns the json representation """
-    return json.dumps(my_obj)
+    return (json.dumps(my_obj))
